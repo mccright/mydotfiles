c3b500b94e7ec2a8d9c7cee80457d51d5ba09a26
mccright <6339517+mccright@users.noreply.github.com>
2022-01-08 15:15:41 -0600

    create terminal_colors.sh
    This is used by other scripts by sourcing it like: ". ./terminal_colors.sh"

---
+++ bin/terminal_colors.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+### Script Name - terminal_colors.sh
+### Common color codes - include them in other scripts.
+# This list started with one I noticed in: 
+# https://github.com/TheKevJames/tools/blob/master/docker-tuning-primer/root/tuning-primer.sh
+# and I only built out the broader spectrum
+# Example below
+export normal='\033[0m'
+export black='\033[30m'
+export boldblack='\033[1;0m'
+export gray='\033[90m'
+export graybg='\033[100m'
+export lightgray='\033[37m'
+export lightgraybg='\033[47m'
+export white='\033[37m'
+export boldwhite='\033[1;37m'
+export red='\033[31m'
+export redbg='\033[41m'
+export lightred='\033[91m'
+export lightredbg='\033[101m'
+export boldred='\033[1;31m'
+export green='\033[32m'
+export greenbg='\033[42m'
+export lightgreen='\033[92m'
+export lightgreenbg='\033[102m'
+export boldgreen='\033[1;32m'
+export yellow='\033[33m'
+export lightyellow='\033[93m'
+export boldyellow='\033[1;33m'
+export yellowbg='\033[43m'
+export lightyellowbg='\033[103m'
+export blue='\033[34m'
+export bluebg='\033[44m'
+export boldblue='\033[1;34m'
+export lightblue='\033[94m'
+export lightbluebg='\033[104m'
+export magenta='\033[35m'
+export boldmagenta='\033[1;35m'
+export cyan='\033[36m'
+export cyanbg='\033[46m'
+export lightcyan='\033[96m'
+export lightcyanbg='\033[106m'
+export boldcyan='\033[1;36m'
