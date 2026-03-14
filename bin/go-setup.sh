8d74faa6eb55631e46a13754d9d176c35e79c845
mccright <6339517+mccright@users.noreply.github.com>
2026-03-14 12:07:41 -0500

    Update go-setup.sh
    Added latest version, corrected download URL and added some comments

--- bin/go-setup.sh
+++ bin/go-setup.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 # Thank you Tom Hudson
 # https://github.com/tomnomnom/dotfiles/blob/master/go-setup.sh
+# and https://go.dev/doc/install
 # VERSION="1.18"
-# Updated my std version 21-May-2023
-VERSION="1.20.4"
+# VERSION="1.20.4"
+# VERSION="1.24.6"
+VERSION="1.26.1"
 
 source .bashrc
-wget https://storage.googleapis.com/golang/go$VERSION.linux-amd64.tar.gz
+# download the specified golang version
+wget https://go.dev/dl/go$VERSION.linux-amd64.tar.gz
+# extract the archive you just downloaded
 tar xvfz go$VERSION.linux-amd64.tar.gz
+# Do not untar the archive into an existing /usr/local/go tree. 
+# This is known to produce broken Go installations.
+# remove previous Go installation by deleting the /usr/local/go folder (if it exists)
 sudo rm -rf /usr/local/go
+# Move the newly-extracted files creating a fresh Go tree in /usr/local/go
 sudo mv go /usr/local/
-
+# Get latest tools and packages for development & static analysis
 go install golang.org/x/tools/cmd/goimports@latest
+# Get the latest autocompletion daemon for the Go programming language
 go install github.com/nsf/gocode@latest
-
+# Cleanup by deleting the just-downloaded golang archive
 rm go$VERSION.linux-amd64.tar.gz
